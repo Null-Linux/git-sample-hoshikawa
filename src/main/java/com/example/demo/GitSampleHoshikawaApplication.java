9464fcf30816334f44e728fb5d6d4945ef95d248
takahiro.hoshikawa <takahiro.hoshikawa@rakus.co.jp>
2017-06-14 13:59:10 +0900

    コメントに猫カフェ行きてえを追加

--- src/main/java/com/example/demo/GitSampleHoshikawaApplication.java
+++ src/main/java/com/example/demo/GitSampleHoshikawaApplication.java
@@ -9,5 +9,5 @@ public class GitSampleHoshikawaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GitSampleHoshikawaApplication.class, args);
 	}
-	//aauuuu
+	//猫カフェ行きてえ
 }
